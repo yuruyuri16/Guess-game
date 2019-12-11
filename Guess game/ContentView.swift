d0fb0a13f1fb679296a93760a2654bb47bda05d2
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2019-12-12 01:11:46 +0200

    Initial Commit

---
+++ Guess game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess game
+//
+//  Created by Miguel Yurivilca on 12/12/19.
+//  Copyright © 2019 Miguel Yurivilca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
